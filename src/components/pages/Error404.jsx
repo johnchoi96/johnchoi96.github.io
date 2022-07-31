4ea998b0eb810f5e325334cbde40f3153e076e52
John Choi <johnchoi1003@icloud.com>
2022-07-31 14:21:23 -0400

    404 page change

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import errorImage from '../../assets/images/error404_meme.jpg'
 
 const Error404 = () => {
     return (
         <div>
             <h1>Wrong page!</h1>
             <p>This page does not exist!</p>
+            <img src={errorImage} alt='404 meme' />
         </div>
     )
 }
