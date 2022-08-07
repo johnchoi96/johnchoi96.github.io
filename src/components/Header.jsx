dd3588171f8425f585d01e074d1a6851afcc7a7f
John Choi <johnchoi1003@icloud.com>
2022-08-07 12:54:24 -0400

    Github dropdown for mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ import {
 } from 'react-router-dom'
 import {BrowserView, MobileView} from 'react-device-detect'
 
+import './Header.styles.scss'
+
 const Header = () => {
     return (
         <div>
@@ -34,8 +36,8 @@ const Header = () => {
                 </header>
             </BrowserView>
             <MobileView>
-                <header class="collapse" id="navbarToggle">
-                    <div class="bg-dark p-4">
+                <header className="mobile-header collapse align-items-start justify-content-start" id="navbarToggle">
+                    <div className="bg-dark p-4">
                         <span className='nav-link fw-bold text-white'>
                             <a className='nav-link' href='/'>
                                 <h4>Home</h4>
@@ -44,11 +46,6 @@ const Header = () => {
                         <span className='nav-link text-white-50'>
                             <a className='nav-link' href='/myworkspace'>
                                 <h6>My Workspace</h6>
-                            </a>
-                        </span>
-                        <span className='nav-link text-white-50'>
-                            <a className='nav-link' href='https://www.github.com/johnchoi96'>
-                                <h6>GitHub</h6>
                             </a>
                         </span>
                         <span className='nav-link text-white-50'>
@@ -66,12 +63,23 @@ const Header = () => {
                                 <h6>Resume</h6>
                             </a>
                         </span>
+                        <div className="nav-link d-flex justify-content-center align-items-center">
+                            <li className="nav-item dropdown">
+                                <button className="nav-link dropdown-toggle" id="navbarScrollingDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                    GitHub
+                                </button>
+                                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li><a className="dropdown-item" href='https://www.github.com/johnchoi96'>GitHub</a></li>
+                                    <li><a className="dropdown-item" href='https://github.com/johnchoi96/johnchoi96.github.io'>Source</a></li>
+                                </ul>
+                            </li>
+                        </div>
                     </div>
                 </header>
-                <nav class="navbar navbar-dark bg-dark">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
                     </div>
                 </nav>
