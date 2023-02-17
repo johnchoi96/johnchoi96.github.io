828777775bd27364a4c9f9b86c842ba2a000f0ef
John Choi <johnchoi1003@icloud.com>
2023-02-16 23:24:46 -0500

    Best I can do

--- src/pages/diplomapage/DiplomaPage.jsx
+++ src/pages/diplomapage/DiplomaPage.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 
 export default function DiplomaPage() {
     const tosu_bs_cse = '/assets/files/tOSU_BSCSE_Diploma_JohnChoi.pdf'
+
+    const ratio = 1.5
+    const length1 = Math.max(window.innerWidth, window.innerHeight) * 0.4
+    const length2 = length1 * ratio
+
     return (
-        <div className='diploma-body' style={{
-                backgroundColor: '#A7B1B7'
+        <div style={{
+                paddingTop: '44px',
+                backgroundColor: '#A7B1B7',
+                minHeight: (window.innerHeight - 44)
             }}>
             <p>The Ohio State University B.S Computer Science and Engineering</p>
-            <embed src={process.env.PUBLIC_URL + tosu_bs_cse} width='800px' height='600px' />
+            <embed
+                src={process.env.PUBLIC_URL + tosu_bs_cse}
+                width={Math.max(length1, length2)}
+                height={Math.min(length1, length2)}
+            />
         </div>
     )
 }
