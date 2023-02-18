d7d75988548309d930d5018db72691217a6d5a63
John Choi <johnchoi1003@icloud.com>
2023-02-18 01:07:22 -0500

    Indentation fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
             <div className='navbar-height container-fluid'>
                 <NavLink className='navbar-brand nav-link fw-bold' to='/'>Home</NavLink>
                 <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-                <span className='navbar-toggler-icon'></span>
+                    <span className='navbar-toggler-icon'></span>
                 </button>
                 <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                     <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
