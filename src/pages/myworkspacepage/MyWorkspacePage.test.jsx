1f085e398a74c7efc060fe3cd4405c32bf2061e1
John Choi <johnchoi1003@icloud.com>
2023-02-27 02:02:51 -0500

    Code format

--- src/pages/myworkspacepage/MyWorkspacePage.test.jsx
+++ src/pages/myworkspacepage/MyWorkspacePage.test.jsx
@@ -1,10 +1,9 @@
-
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
 import MyWorkspacePage from './MyWorkspacePage'
 import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import '../../setupTests'
-import { Button } from '@mui/material';
+import { Button } from '@mui/material'
 
 configure({ adapter: new Adapter() })
 // Enzyme.configure({ adapter: new Adapter() })
@@ -14,9 +13,15 @@ test('My Workspace loads', async () => {
 
     render(<MyWorkspacePage />)
     // expect modal display
-    const modalHeadingText = screen.getByRole('heading', {name: "This page is being worked on!"})
+    const modalHeadingText = screen.getByRole('heading', {
+        name: 'This page is being worked on!'
+    })
     expect(modalHeadingText).toBeInTheDocument()
-    const modal = shallow(<Button id='button' onClick={mockCallBack} variant='contained'>OK</Button>)
+    const modal = shallow(
+        <Button id='button' onClick={mockCallBack} variant='contained'>
+            OK
+        </Button>
+    )
     modal.find('#button').simulate('click')
     expect(mockCallBack.mock.calls.length).toEqual(1)
 })
