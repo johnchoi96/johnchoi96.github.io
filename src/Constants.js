c85366af133529a6c3b221f1b16bf0e88462853f
John Choi <johnchoi1003@icloud.com>
2023-03-23 11:13:27 -0400

    Code cleanup

--- src/Constants.js
+++ src/Constants.js
@@ -1,13 +1,8 @@
-const prod = {
+
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://ec2-3-15-212-234.us-east-2.compute.amazonaws.com:8080/'
+
+export const config = {
     endpoint: {
-        email: 'http://ec2-3-15-212-234.us-east-2.compute.amazonaws.com:8080/api/email/send',
+        email: `${url}api/email/send`,
     }
 }
-
-const dev = {
-    endpoint: {
-        email: 'http://localhost:8080/api/email/send'
-    }
-}
-
-export const config = process.env.NODE_ENV === 'development' ? dev : prod
