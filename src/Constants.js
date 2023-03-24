bfec6780cf508de6355a60c47298fa85410e59e0
John Choi <johnchoi1003@icloud.com>
2023-03-24 12:57:29 -0400

    Test

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,8 @@
 
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://ec2-3-15-212-234.us-east-2.compute.amazonaws.com:8080/'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'https://cors-anywhere.herokuapp.com/http://ec2-3-15-212-234.us-east-2.compute.amazonaws.com:8080/'
 
 export const config = {
     endpoint: {
-        email: `${url}api/email/send`,
+        email: `${url}api/email/send/`,
     }
 }
