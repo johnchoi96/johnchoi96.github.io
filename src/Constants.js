de5379fe0d379f71efef0fd1a103b0fc1b9dc2a1
John Choi <johnchoi1003@icloud.com>
2023-04-09 22:57:37 -0400

    .

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,8 @@
 
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'https://ec2-18-217-126-86.us-east-2.compute.amazonaws.com:8080/'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://ec2-18-217-126-86.us-east-2.compute.amazonaws.com:8080'
 
 export const config = {
     endpoint: {
-        email: `${url}api/email/send/`,
+        email: `${url}/api/test/ping`,
     }
 }
