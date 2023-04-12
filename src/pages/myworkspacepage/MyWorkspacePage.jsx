e3e675f8a00aee460033a05290fbefd9c69bf6c1
John Choi <johnchoi1003@icloud.com>
2023-04-12 12:14:25 -0400

    added a space

--- src/pages/myworkspacepage/MyWorkspacePage.jsx
+++ src/pages/myworkspacepage/MyWorkspacePage.jsx
@@ -52,7 +52,7 @@ export default function MyWorkspacePage({ canScroll }) {
                     backgroundColor: getBackgroundColor()
                 }}
             >
-                <h1 className={`text-${getFontColorText()}`}>MyWorkspace</h1>
+                <h1 className={`text-${getFontColorText()}`}>My Workspace</h1>
 
                 <div className='container mt-5'>
                     <div className='row row-cols-1 row-cols-md-2 g-4'>
