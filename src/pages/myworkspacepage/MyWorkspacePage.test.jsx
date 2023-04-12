80ce112acd7db72424e20e0455e208b8d2e13f48
John Choi <johnchoi1003@icloud.com>
2023-04-12 10:55:21 -0400

    Removed BubbleUI related test

--- src/pages/myworkspacepage/MyWorkspacePage.test.jsx
+++ src/pages/myworkspacepage/MyWorkspacePage.test.jsx
@@ -1,27 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import MyWorkspacePage from './MyWorkspacePage'
-import { shallow, configure } from 'enzyme'
+import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import '../../setupTests'
-import { Button } from '@mui/material'
+import { MemoryRouter } from 'react-router-dom'
 
 configure({ adapter: new Adapter() })
-// Enzyme.configure({ adapter: new Adapter() })
 
 test('My Workspace loads', async () => {
-    const mockCallBack = jest.fn()
-
-    render(<MyWorkspacePage />)
-    // expect modal display
-    const modalHeadingText = screen.getByRole('heading', {
-        name: 'This page is being worked on!'
-    })
-    expect(modalHeadingText).toBeInTheDocument()
-    const modal = shallow(
-        <Button id='button' onClick={mockCallBack} variant='contained'>
-            OK
-        </Button>
-    )
-    modal.find('#button').simulate('click')
-    expect(mockCallBack.mock.calls.length).toEqual(1)
+    render(<MyWorkspacePage />, {wrapper: MemoryRouter})
+    const headingText = screen.getByRole('heading', {name: 'MyWorkspace'})
+    expect(headingText).toBeInTheDocument()
 })
