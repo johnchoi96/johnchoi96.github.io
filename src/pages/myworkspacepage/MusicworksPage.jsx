bc2a175ea42810e0bc1a2dc6ee78fc208be33034
John Choi <johnchoi1003@icloud.com>
2023-04-11 20:21:24 -0400

    Two audios imported and implemented

--- src/pages/myworkspacepage/MusicworksPage.jsx
+++ src/pages/myworkspacepage/MusicworksPage.jsx
@@ -1,17 +1,54 @@
 import React from 'react'
 import { getBackgroundColor } from '../../Utils/colorUtils'
 
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
+
+import { getFontColorText } from '../../Utils/colorUtils'
+import playlist from '../../assets/music-playlist.json'
+
+function MusicPlayer({ src }) {
+    return (
+        <div>
+            <AudioPlayer src={src} volume={0.5} />
+        </div>
+    )
+}
+
+function MusicCard({ title, src }) {
+    return (
+        <div>
+            <h4 className={`text-${getFontColorText()}`}>{title}</h4>
+            <MusicPlayer src={src} />
+        </div>
+    )
+}
 
 export default function MusicworksPage() {
+    const { musics } = playlist
+
     return (
         <div
+            className='container'
             style={{
                 paddingTop: '44px',
                 backgroundColor: getBackgroundColor(),
                 minHeight: window.innerHeight - 44
             }}
         >
-            <ReactAudioPlayer />
+            <div className='row row-cols-1 row-cols-md-3 g-4'>
+                {musics.map((music, i) => {
+                    return (
+                        <div className='col-lg-4 mb-3' key={i}>
+                            <MusicCard
+                                className='card'
+                                title={music.title}
+                                src={music.src}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
