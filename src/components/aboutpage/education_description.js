f55218ece3b09ec72d59172bc9e84f863e4711c0
John Choi <johnchoi1003@icloud.com>
2023-12-21 00:20:43 -0500

    Added osu and gatech tooltips

---
+++ src/components/aboutpage/education_description.js
@@ -0,0 +1,7 @@
+
+export const osuDescription = `Software engineering specialization is the
+area most people think of when they hear the phrase “computer science.” It encompasses
+programming languages, software development (including coding practices), analysis,
+testing and debugging, optimization, and scalability among many areas. Source: https://cse.osu.edu`
+
+export const gatechDescription = `OMSCS Starting 2024. Specialization TBD.`
