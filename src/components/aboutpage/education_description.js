a3442abdb2de24562ce172f4ab07552f8f0a9565
John Choi <33343507+johnchoi96@users.noreply.github.com>
2025-04-01 19:13:00 -0400

    Update education_description.js

--- src/components/aboutpage/education_description.js
+++ src/components/aboutpage/education_description.js
@@ -4,4 +4,4 @@ area most people think of when they hear the phrase “computer science.” It e
 programming languages, software development (including coding practices), analysis,
 testing and debugging, optimization, and scalability among many areas. Source: https://cse.osu.edu`
 
-export const gatechDescription = `OMSCS Aug 2024 - Present. Computing Systems Specialization.`
+export const gatechDescription = `OMSCS Aug 2024 - Present. Artificial Intelligence Specialization.`
